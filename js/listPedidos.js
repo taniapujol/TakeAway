d2f15195089239acaf74db16e789786dc1dd34da
ADP02-PC09\ADP <ainatpujolrigual@gmail.com>
2017-03-10 17:52:46 +0100

    preparando area pedidos (html,php y js)

---
+++ js/listPedidos.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var debug = true;
+   
+	    $.ajax({
+	    	url: '../php/getListPedido.php',
+	    	type: 'GET',
+	    	dataType: 'json',
+	    	success: function(result){
+	    		if (debug) console.log(result.query);
+	    		var tbl_body = "";
+	    		
+	    		$.each(result.query, function(index, val) {
+	    		var tbl_row = "";
+	    			$.each(this, function(index, val) {
+	    				 
+	    				 tbl_row +="<td>" + val + "</td>"
+	    			});
+	    				tbl_body += "<tr>" + tbl_row +"</tr>";
+	    		});
+	    		$("#listado tbody").html(tbl_body);
+	    	},
+	    	error: function(result){
+	    		alert('errorororor');
+	    	}
+	  });	
+});
+    
--- php/envioCat.php
+++ php/envioCat.php
@@ -8,34 +8,6 @@ if ($_SERVER['REQUEST_METHOD']==='POST'){
 		$campos .= $value['name'] . ',';
 		$valores .= $value['value'] . "','";
 	}
-	if ($campos === 'foto'){
-		// Recibo los datos de la imagen
-		$nombre_img = $_FILES['imagen']['foto'];
-		$tipo = $_FILES['imagen']['type'];
-		$tamano = $_FILES['imagen']['size'];
-	 
-		//Si existe imagen y tiene un tamaño correcto
-		if (($nombre_img == !NULL) && ($_FILES['foto']['size'] <= 200000)) 
-			{
-		   		//indicamos los formatos que permitimos subir a nuestro servidor
-		   		if (($_FILES["foto"]["type"] == "foto/gif")
-		   		|| ($_FILES["foto"]["type"] == "foto/jpeg")
-		   		|| ($_FILES["foto"]["type"] == "foto/jpg")
-		   		|| ($_FILES["foto"]["type"] == "foto/png"))
-		   	{
-		      // Ruta donde se guardarán las imágenes que subamos
-		      $directorio = $_SERVER['DOCUMENT_ROOT'].'/xampp/htdocs/tania/TakeAway/img/categorias';
-		      // Muevo la imagen desde el directorio temporal a nuestra ruta indicada anteriormente
-		      move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$nombre_img);
-		    } else {
-		       //si no cumple con el formato
-		       echo "No se puede subir una imagen con ese formato ";
-		    }
-		} else {
-		   //si existe la variable pero se pasa del tamaño permitido
-		   if($nombre_img == !NULL) echo "La imagen es demasiado grande "; 
-		}
-	}
 	$campos = substr($campos,0,-1);
 	$valores = substr($valores,0,-2);
 	$sql = "INSERT INTO categoria ($campos) VALUES ($valores)";
